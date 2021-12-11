db72b921fc1e7658837f6bddd9aea8b423ee48aa
Aloui Soumaya <69814778+Aloui-Soumaya@users.noreply.github.com>
2021-12-11 22:27:04 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -14,4 +14,27 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
+    public String Reverse (String chaine){
+        String res="";
+        for(int i=0;i<chaine.length();i++)
+            res=chaine.charAt(i)+res;
+        return res;
+    }
+    public char MinChar(String chaine){
+        char min=chaine.charAt(0);
+        for(int i=1;i<chaine.length();i++)
+            if (chaine.charAt(i)<min)
+                min=chaine.charAt(i);
+        return min;
+    }
+    public String CaseChanger(String chaine){
+        String res="";
+        for(int i=0;i<chaine.length();i++){
+            if(Character.isLowerCase(chaine.charAt(i)))
+                res=res+Character.toUpperCase(chaine.charAt(i));
+            else if (Character.isUpperCase(chaine.charAt(i)))
+                res=res+Character.toLowerCase(chaine.charAt(i));
+        }
+        return res;
+    }
 }
