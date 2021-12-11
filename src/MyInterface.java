b3f8fa5a1c8f9c9031adfbdf3d4759f2e7caf55c
Aloui Soumaya <69814778+Aloui-Soumaya@users.noreply.github.com>
2021-12-11 22:18:44 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
+    public String Reverse (String chaine) throws RemoteException;
+    public String CaseChanger(String chaine) throws  RemoteException;
+    public char MinChar(String chaine) throws  RemoteException;
 
 }
