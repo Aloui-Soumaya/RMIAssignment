a080478c85e29242efb992d07e77ab25770788b4
Aloui Soumaya <69814778+Aloui-Soumaya@users.noreply.github.com>
2021-12-11 22:21:07 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -2,10 +2,17 @@ import java.rmi.Naming;
 
 public class Client {
 
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         try {
-           MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
-
+            MyInterface myService = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            Scanner scanner =new Scanner(System.in);
+            String myString= scanner.nextLine();
+            System.out.println("the reverse of that string : ");
+            System.out.println(myService.Reverse(myString));
+            System.out.println("the character that has the minimum ASCII value : ");
+            System.out.println(myService.MinChar(myString));
+            System.out.println("the result of CaseChanger :");
+            System.out.println(myService.CaseChanger(myString));
         } catch (Exception e) {
             e.printStackTrace();
         }
